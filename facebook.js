4b37f7a7f86899af643b61f8342bdfd7f2fccf4b
Adewale-alt <78614131+Adewale-alt@users.noreply.github.com>
2021-02-17 02:44:08 +0100

    Add files via upload

---
+++ facebook.js
@@ -0,0 +1,29 @@
+let likebtn = document.querySelector('.likebutton')
+let input = document.querySelector('.input')
+likebtn.addEventListener('click', ()=> {
+  input.value = parseInt(input.value) + 1;
+})
+
+let sharebtn = document.querySelector('.sharebutton')
+let input2 = document.querySelector('.input2')
+sharebtn.addEventListener('click', ()=> {
+  input2.value = parseInt(input2.value) + 1;
+})
+//////////
+
+
+
+let likebtn2 = document.querySelector('.likebutton2')
+let input3 = document.querySelector('.input3')
+likebtn2.addEventListener('click', ()=> {
+  input3.value = parseInt(input3.value) + 1;
+})
+
+let sharebtn2 = document.querySelector('.sharebutton2')
+let input4 = document.querySelector('.input4')
+sharebtn2.addEventListener('click', ()=> {
+  input4.value = parseInt(input4.value) + 1;
+})
+
+
+
